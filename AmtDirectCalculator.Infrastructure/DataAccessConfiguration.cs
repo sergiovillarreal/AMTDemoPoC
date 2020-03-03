dec1cef87675a8274a3d24862eff9dc05e196ed1
sergiovillarreal <sergio.villarreal@levvel.io>
2020-03-03 13:06:03 -0500

    ignore code coverage for model classes

--- AmtDirectCalculator.Infrastructure/DataAccessConfiguration.cs
+++ AmtDirectCalculator.Infrastructure/DataAccessConfiguration.cs
@@ -1,7 +1,9 @@
-﻿using AmtDirectCalculator.SharedKernel.Interfaces;
+﻿using System.Diagnostics.CodeAnalysis;
+using AmtDirectCalculator.SharedKernel.Interfaces;
 
 namespace AmtDirectCalculator.Core.Entities
 {
+    [ExcludeFromCodeCoverage]
     public class DataAccessConfiguration: IDataAccessConfiguration
     {
         public string BaseUrl { get; set; }
